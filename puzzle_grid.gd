49213c15ba569d4c76f25949b28bb46d55a03527
rybadour <rybadour@gmail.com>
2025-12-28 23:28:07 -0500

    Created a dedicated puzzle grid component
     - Puzzle grid can be small sized or life sized and supports arbitrary number of colums and rows.

--- puzzle_grid.gd
+++ puzzle_grid.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 @export var numRows = 1
 @export var numCols = 1
+@export var dollHouseScale = false;
 
 @onready var gridBase = $GridBase
 @onready var baseBody: CSGBox3D = $GridBase/Base
@@ -13,12 +14,16 @@ extends Node3D
 func _ready() -> void:
 	set_base_size()
 	
+	var scale = getScale()
 	for z in range(numRows):
 		for x in range(numCols):
 			var cell: CSGBox3D = cellTemplate.duplicate()
-			cell.position.x = 0.05 + (x * 0.1)
-			cell.position.y = 0.03
-			cell.position.z = 0.05 + (z * 0.1)
+			cell.position.x = scale/2 + (x * scale)
+			cell.position.y = 0.03 if dollHouseScale else 0.5
+			cell.position.z = scale/2 + (z * scale)
+			cell.size.x = scale * 0.9
+			cell.size.y = 0.02 if dollHouseScale else 0.8
+			cell.size.z = scale * 0.9
 			cell.visible = true
 			gridBase.add_child(cell)
 		
@@ -28,7 +33,11 @@ func _process(delta):
 		return
 
 func set_base_size():
-	baseBody.size.x = numCols * 0.1
-	baseBody.size.z = numRows * 0.1
+	baseBody.size.x = numCols * getScale()
+	baseBody.size.y = 0.05 if dollHouseScale else 0.4
+	baseBody.size.z = numRows * getScale()
 	baseBody.position.x = baseBody.size.x / 2
 	baseBody.position.z = baseBody.size.z / 2
+	
+func getScale():
+	return 0.1 if dollHouseScale else 4
